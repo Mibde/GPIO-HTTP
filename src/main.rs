434598be1b0e0a8967c65f8326570ed0837ed3b2
Paulo-21 <paul.cibier@gmail.com>
2022-08-29 17:37:55 +0200

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,57 @@
-
+use rppal::gpio::Gpio;
+use rppal::gpio;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let body = reqwest::blocking::get("https://mibde.fr/update_door").unwrap()
-    .text().unwrap();
 
-    println!("body = {:?}", body);
+        let answer = Arc::new(Mutex::new(gpio::Level::Low));
+    let answer_ref = answer.clone();
+    let answer_ref2 = answer.clone();
 
+let t = thread::spawn(move || {
+        let gpio = Gpio::new().unwrap();
+        let pin_analog = gpio.get(23).unwrap().into_input();
+
+        loop {
+
+        let mut answer = answer_ref.lock().unwrap();
+        *answer = pin_analog.read();
+
+        drop(answer);
+        thread::sleep(Duration::from_secs(1));
+
+        }
+});
+let t2 = thread::spawn(move || {
+
+let mut last = gpio::Level::Low;
+    loop {
+
+        let answer = answer_ref2.lock().unwrap();
+        if *answer != last {
+                last = *answer;
+                if last == gpio::Level::Low {
+                let _body = reqwest::blocking::get("https://mibde.fr/door&door=open").unwrap()
+                    .text();
+                }
+                else {
+                        let _body = reqwest::blocking::get("https://mibde.fr/door&door=close").unwrap()
+                    .text();
+
+                }
+
+        //println!("Second tread : {}", answer);
+        }
+                drop(answer);
+            thread::sleep(Duration::from_secs(1));
+
+        }
+    });
+
+
+t.join().unwrap();
+t2.join().unwrap();
 }
