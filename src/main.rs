a7f1e887fbf6e7b862da159690f1ac8dbf1d961a
Paulo-21 <paul.cibier@gmail.com>
2022-09-14 23:12:58 +0200

    new implementation

--- src/main.rs
+++ src/main.rs
@@ -7,41 +7,24 @@ use std::time::Duration;
 
 fn main() {
 
-    let answer = Arc::new(Mutex::new(gpio::Level::Low));
-    let answer_ref = answer.clone();
-    let answer_ref2 = answer.clone();
+    let state = gpio::Level::Low;
+    let mut last = gpio::Level::Low;
+    
+    let gpio = Gpio::new().unwrap();
+    let pin_analog = gpio.get(23).unwrap().into_input();
 
-    let t = thread::spawn(move || {
-        let gpio = Gpio::new().unwrap();
-        let pin_analog = gpio.get(23).unwrap().into_input();
-
-        loop {
-            let mut answer = answer_ref.lock().unwrap();
-            *answer = pin_analog.read();
-            drop(answer);
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-    let t2 = thread::spawn(move || {
-
-        let mut last = gpio::Level::Low;
-        loop {
-            let answer = answer_ref2.lock().unwrap();
-            if *answer != last {
-                last = *answer;
-                if last == gpio::Level::Low {
-                    let _body = reqwest::blocking::get("https://mibde.fr/door&door=open");
-                }
-                else {
-                    let _body = reqwest::blocking::get("https://mibde.fr/door&door=close");
-                }
-                //println!("Second tread : {}", answer);
+    loop {
+        state = pin_analog.read();
+        if state != last {
+            last = state;
+            if last == gpio::Level::Low {
+                let _body = reqwest::blocking::get("https://mibde.fr/door&door=open");
             }
-                drop(answer);
-                thread::sleep(Duration::from_secs(1));
+            else {
+                let _body = reqwest::blocking::get("https://mibde.fr/door&door=close");
+            }
         }
-    });
-
-    t.join().unwrap();
-    t2.join().unwrap();
+    
+    }
+   
 }
