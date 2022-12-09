e9a7b3764bcb8aa8b3cc7897a157e17ea3c8014c
Paulo-21 <paul.cibier@gmail.com>
2022-12-09 19:07:44 +0100

    handle the case when the wifi is not started yet

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
+use std::time::Duration;
+
+use reqwest::blocking::Response;
+use reqwest::Error;
 use rppal::gpio::Gpio;
 use rppal::gpio;
+use rppal::spi::Error;
 
-fn send(pin : gpio::Level) {
+fn send(pin : gpio::Level) -> Result<Response, Error> {
     match pin {
-        gpio::Level::Low => { let _body = reqwest::blocking::get("https://mibde.fr/websocketDoor/?door=open");},
-        _ => {let _body = reqwest::blocking::get("https://mibde.fr/websocketDoor/?door=close");},
+        gpio::Level::Low => reqwest::blocking::get("https://mibde.fr/websocketDoor/?door=open"),
+        _ => reqwest::blocking::get("https://mibde.fr/websocketDoor/?door=close"),
+    }
+}
+fn init(pin : gpio::Level) {
+    let un_second = Duration::from_millis(500);
+    loop {
+        match send(pin) {
+            Ok(x) => {
+                break;
+            },
+            Err(err) => {
+                thread::sleep(un_second);
+            }
+        }
     }
 }
 fn main() {
     let gpio = Gpio::new().unwrap();
     let mut pin_analog = gpio.get(23).unwrap().into_input().read();
-    send(pin_analog);
+    init(pin_analog);
     let mut last = pin_analog;
+    let un_second = Duration::from_millis(500);
     loop {
         pin_analog = gpio.get(23).unwrap().into_input().read();
         if pin_analog != last {
             send(pin_analog);
             last = pin_analog;
         }
+        thread::sleep(un_second);
     }
 }
