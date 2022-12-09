03081f78326fe1de096e80891eebf85f6593d2b2
Paulo-21 <paul.cibier@gmail.com>
2022-12-09 15:04:54 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,22 @@
 use rppal::gpio::Gpio;
 use rppal::gpio;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
-use std::time::Duration;
 
+fn send(pin : gpio::Level::Low) {
+    match pin {
+        gpio::Level::Low => { let _body = reqwest::blocking::get("https://mibde.fr/door&door=open");},
+        _ => let _body = {reqwest::blocking::get("https://mibde.fr/door&door=close");},
+    }
+}
 fn main() {
-
-    let answer = Arc::new(Mutex::new(gpio::Level::Low));
-    let answer_ref = answer.clone();
-    let answer_ref2 = answer.clone();
-
-    let t = thread::spawn(move || {
-        let gpio = Gpio::new().unwrap();
-        let pin_analog = gpio.get(23).unwrap().into_input();
-
-        loop {
-            let mut answer = answer_ref.lock().unwrap();
-            *answer = pin_analog.read();
-            drop(answer);
-            thread::sleep(Duration::from_secs(1));
+    let gpio = Gpio::new().unwrap();
+    let mut pin_analog = gpio.get(23).unwrap().into_input();
+    send(pin_analog);
+    let last = pin_analog;
+    loop {
+        pin_analog = gpio.get(23).unwrap().into_input();
+        if pin_analog != last {
+            send(pin_analog);
+            last = pin_analog;
         }
-    });
-    let t2 = thread::spawn(move || {
-
-        let mut last = gpio::Level::Low;
-        loop {
-            let answer = answer_ref2.lock().unwrap();
-            if *answer != last {
-                last = *answer;
-                if last == gpio::Level::Low {
-                    let _body = reqwest::blocking::get("https://mibde.fr/door&door=open");
-                }
-                else {
-                    let _body = reqwest::blocking::get("https://mibde.fr/door&door=close");
-                }
-                //println!("Second tread : {}", answer);
-            }
-                drop(answer);
-                thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    t.join().unwrap();
-    t2.join().unwrap();
+    }
 }
